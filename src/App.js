c4148d1a1aa84af7444bc590a04273259a396959
S Doran <sdoran@gmail.com>
2025-02-08 15:37:48 +0000

    Adding custom logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './assets/img/logo_sd.png';
+import './assets/css/App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="SDORAN logo" />
         <p> 
           SDoran.com | Sarah Doran | Engineering Leader @Travelport
         </p>
