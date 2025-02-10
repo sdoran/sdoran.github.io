737cf9c5320cda8a56f288b555fd74790df0615b
S Doran <sdoran@gmail.com>
2025-02-10 19:06:34 +0000

    removing test menu. adding linkedin link

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import logo from './assets/img/logo_sd.png';
 import './assets/css/App.css';
-import Menu from './components/Menu'; // Import the Menu component
+//import Menu from './components/Menu'; // Import the Menu component
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <Menu /> {/* Add the Menu component */}
+      {/* <Menu /> Add the Menu component */}
         <img src={logo} className="App-logo" alt="SDORAN logo" />
+        <h2>SDORAN.com</h2>
         <p> 
-          SDoran.com | Sarah Doran | Engineering Leader @Travelport
+          Sarah Doran | Engineering Leader @Travelport
         </p>
+        <p>LinkedIn: <a href='https://www.linkedin.com/in/sarahdoran/'>https://www.linkedin.com/in/sarahdoran/</a></p>
+       
       </header>
     </div>
   );
