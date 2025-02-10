29b10da1653ce34ad43c9aa0a4fce082f287d7bf
S Doran <sdoran@gmail.com>
2025-02-10 18:48:22 +0000

    package

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './assets/img/logo_sd.png';
 import './assets/css/App.css';
+import Menu from './components/Menu'; // Import the Menu component
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <Menu /> {/* Add the Menu component */}
         <img src={logo} className="App-logo" alt="SDORAN logo" />
         <p> 
           SDoran.com | Sarah Doran | Engineering Leader @Travelport
